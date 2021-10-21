c800fab622fbbb15386a6e711ded4892f86f76fe
eduardohor <eduardo.hor@outlook.com>
2021-10-21 09:34:15 -0300

    alterate app

---
+++ nlw-app/src/components/UserPhoto/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Image} from 'react-native';
+
+import { styles } from './styles';
+
+const SIZES = {
+  SMALL:{
+    containerSize: 32,
+    avatarSize: 28
+  },
+   NORMAL:{
+    containerSize: 32,
+    avatarSize: 28
+  }
+}
+
+type Props ={
+  imageUri: string | undefined,
+  sizes?: 'SMALL' | 'NORMAL'
+
+}
+
+export function UserPhoto({imageUri, sizes = 'NORMAL'}: Props){
+  const {containerSize, avatarSize} = SIZES[sizes]
+
+  return (
+   <Image
+    source={{uri: 'https://github.com/eduardohor.png'}}
+    style={styles.avatar}
+    />
+  );
+}
---
+++ nlw-app/src/components/UserPhoto/styles.ts
@@ -0,0 +1,9 @@
+import { COLORS } from './../../theme/colors';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  avatar: {
+    width: 48,
+    height: 48
+  }
+});
